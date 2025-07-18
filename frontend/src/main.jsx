5ab7023615f09ad82f9faaf2c29c43059e9c9709
Panchenko Denis <Pandenisko@gmail.com>
2025-07-18 22:06:38 +0300

    Added Docker
    Added Docker and config files

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -9,15 +9,11 @@ import App from "./components/App/App.jsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-
           <BrowserRouter>
             <App />
           </BrowserRouter>
-
       </PersistGate>
     </Provider>
-  </StrictMode>
 );
